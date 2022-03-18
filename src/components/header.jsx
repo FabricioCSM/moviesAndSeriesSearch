2161c2317ddfa5b9c6ba4c5b7623fbdc66f4e3fd
Fabricio Martins <fabriciocsmartins@gmail.com>
2022-03-18 17:26:38 -0300

    [feat] Implementa Header com botão de profile, searchBar, LogOut

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Container, FormControl, FormGroup, Nav, Navbar, Button } from 'react-bootstrap';
+import { Container, FormControl, Form, Nav, Navbar, Button } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
-
-// import profileIcon from '../images/profileIcon.svg';
-// import searchIcon from '../images/searchIcon.svg';
+import profileIcon from '../assets/profileIcon.svg';
+import SearchBar from './searchBar';
 
 
 function Header() {
   const history = useHistory();
 
   const [showSearch, setShowSearch] = useState(false);
+  const [warning, setWarning] = useState(false);
 
   const [searchConfigs, setSearchConfigs] = useState({
     typedValue: '',
@@ -35,56 +35,44 @@ function Header() {
   };
 
   const handleBtnClick = () => {
-    const { typedValue } = searchConfigs;
+    // history.push('/profile');
     
   };
 
   return (
-    <Navbar bg="success" expand="sm">
-      <Container>
-        <Link to="/profile">
-          <img
-            // src={ profileIcon }
-            alt="icone de perfil"
-          />
-        </Link>
-   
-        <Navbar.Toggle
-          onClick={ () => setShowSearch(!showSearch) }
-          aria-controls="basic-navbar-nav"
-        >
-          <img
-            // src={ searchIcon }
-            alt="icone de busca"
-            data-testid="search-top-btn"
-          />
-        </Navbar.Toggle>
+    <div>
+      <header className='py-4 bg-dark text-white' style={{width: '100%',display: 'flex', flexDirection: 'row', alignContent: 'space-around'}}>
+        <div className='d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start' >
+          <button className='nav-link text-secondary' style={{borderRadius: '30px'}}
+            onClick={ handleBtnClick}
+          >
+            <img
+              src={ profileIcon }
+              alt="icone de perfil"
+              style={{margin: '1px'}}
+            />
+            Perfil
+          </button>
+        </div>
+        <SearchBar />
+        <div className='d-flex align-items-center justify-content-center' style={{marginLeft: '70%'}}>
+          <button className="btn btn-warning" onClick={ () => setWarning(true) }>
+            Log Out
+          </button>
+        </div>
+      </header>
 
-        { showSearch && (
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <FormGroup>
-                <FormControl
-                  placeholder="Search recipe"
-                  data-testid="search-input"
-                  onChange={ handleChange }
-                />
-              </FormGroup>
-              
-              <Button
-                disabled={ buttonState }
-                data-testid="exec-search-btn"
-                type="button"
-                variant="success"
-                onClick={ handleBtnClick }
-              >
-                Search
-              </Button>
-            </Nav>
-          </Navbar.Collapse>
-        )}
-      </Container>
-    </Navbar>
+      {warning && <div className='modal-content rounded-4 shadow'>
+          <div className='modal-body p-4 text-center'>
+            <h5 className='mb-0'>Você tem certeza que gostaria de sair?</h5>
+          </div>
+          <div className='modal-footer flex-nowrap p-0'>
+            <button className='btn btn-lg text-decoration-none col-6 m-0 rounded-0' onClick={() => history.push('/')}>SIM</button>
+            <button className='btn btn-lg text-decoration-none col-6 m-0 rounded-0' onClick={() => setWarning(false)}>Voltar</button>
+          </div>
+          </div>
+      }
+    </div>
   );
 }
 
